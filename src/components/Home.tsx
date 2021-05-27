2e32fd76b4a4822093a24c48e82b8eaf54a7efef
MrKeon <keon@pubgenius.io>
2021-05-26 20:15:43 -0700

    gutted react stuff and added background color.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,20 +4,7 @@ import logo from '../logo.svg';
 export default function Home() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I love Rachel Sadatian with all of my heart.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
