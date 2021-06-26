0d74baa1cf00546d29c62da69dc1181ece5c4129
MrKeon <keon@pubgenius.io>
2021-06-26 14:53:47 -0700

    added margin fix for iframes.

--- src/components/portfolio/SimplySweet.tsx
+++ src/components/portfolio/SimplySweet.tsx
@@ -1,92 +1,96 @@
 import Brainstorm from './Brainstorm.png';
 import Research from './Research.png';
-
+import './SimplySweet.css';
 const iframeStyle = {
     border: "1px solid rgba(0, 0, 0, 0.1)"
 }
 export default function SimplySweet() {
     return (
-        <div className="SimplySweet">
-            <h2>Simply Sweet Stems Project</h2>
-            <h3>Objective & final deliverables (solo project)</h3>
-            <ul>
-                <li>Written content for 11 webpages (4 pages was the requirement)</li>
-                <li>1 high fidelity prototype (not required)</li>
-                <li>2 completed alternate homepages (not required)</li>
-                <li>1 persona and supporting scenario</li>
-                <li>1 pathway page</li>
-            </ul>
-            <h3>Personal Goal</h3>
-            <p>As an aspiring UX designer, I wanted to pick a theme for this new website that challenges me in terms of aesthetics.</p>
-            <p>I strived to step outside of my comfort zone that is bold and modern with something that is romantic and elegant.</p>
-            <h3>Timeline</h3>
-            <ul>
-                <li>One month</li>
-            </ul>
-            <h3>Tools</h3>
-            <ul>
-                <li>Figma</li>
-                <li>Photoshop</li>
-                <li>Pen</li>
-                <li>Paper</li>
-                <li>Sticky notes</li>
-            </ul>
-            <h3>Problem statement</h3>
-            <p>How do I create a wedding floral website for brides and grooms that meets and exceeds their floral expectations while simultaneously addressing COVID-19 policies and procedures?</p>
-            <h3>Audience</h3>
-            <ul>
-                <li>Brides</li>
-                <li>Grooms</li>
-                <li>Mother/father of the bride/groom</li>
-                <li>Maid of honor</li>
-                <li>Best man</li>
-            </ul>
-            <h3>Research</h3>
-            <ul>
-                <li>Bride and groom wants, needs, & expectations</li>
-                <li>Floral expectations</li>
-                <li>Multiple wedding florist sites fro content</li>
-                <li>Studied the day and the life of a wedding florist</li>
-            </ul>
-            <h3>Concerns</h3>
-            <ul>
-                <li>Different types of florals</li>
-                <li>Floral arrangements</li>
-                <li>Floral freshness</li>
-                <li>If florals are local or not</li>
-                <li>Allergies & pesticides</li>
-                <li>Environmental impact: silk flowers vs fresh, floral foam, etc</li>
-                <li>Covid-19 protocols (in-store pickup, curbside, or delivered)</li>
-            </ul>
-            <h3>Pain points</h3>
-            <ul>
-                <li>Brides</li>
-                <li>Grooms</li>
-                <li>Mother/father of the bride/groom</li>
-                <li>Maid of honor</li>
-                <li>Best man</li>
-            </ul>
-            <h3>Process</h3>
-            <p>I start with a very preliminary brainstorming technique called mind-mapping. I empty all of my initial ideas, thoughts, and objectives by linking key concepts using images and lines. It helps me flush out my ideas and personal biases.</p>
-            <img src={Brainstorm} />
-            <p>Next, I spent the majority of my time researching my audience as it is the foundation for design strategy. A quote that sticks with me states, "Want your users to fall in love with your designs? Fall in love with your users." written by Dana Chisnell. This quote taught me that my sole goal isn't to make websites look "pretty", rather, effectively tailoring users' needs.</p>
-            <img src={Research} />
-            <p>In short, I want to provide users' a solution that balances their needs that is functional yet aesthetically pleasing. I research their emotional state, location, needs, wants, etc. After that, I heavily studied competitor websites. I took the time to write down terms that would interest my audience; terms that they are seeking.</p>
-            <p>In addition, I broke down the hierarchy of the written content page by page, word by word, down to the primary, horizontal, sticky navigation bar. These details matter to me because Jakob's Law states that people spend most of their time using digital products other than yours.</p>
-            <p>Additionally, I studied various types of wedding floral arrangements, different types of wedding florals, studied the branding approach for this new website, and hypothesized business needs.</p>
-            <p>With the time limit I was given and the additional pages I wanted to implement, I still added on new research. I omitted original ideas and continuously referenced the research as a whole, keeping in mind user's needs was at the center of this new website.</p>
-            <p>Lastly, I unfortunately didn’t have the time to create any paper prototypes or low-fidelity prototypes as this was primarily a web writing class.</p>
-            <h3>Next steps</h3>
-            <p>If I were to move forward with this project I would meet with the design team and listen to their recommendations. I would point out that my buttons currently on my proposed designs need tailoring. I would like to ensure the shape, placement, and font size are suited for the audience.</p>
-            <h3>What I learned</h3>
-            <p>I used to lack confidence in my writing and graphic design skills, but not anymore. Pushing myself beyond the required work in all of my classes has made me a solid aspiring UX designer.</p>
-            <p>I learned that my projects are so important to me. While I have made much progress in my studies, I still have so much to learn and that excites me.</p>
-            <h3>Final product</h3>
-            <iframe tabIndex={59} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Flmzx6Z3zw03JWrQqna5fe9%2FPersona-and-Scenario-rachel-Sadatian" allowFullScreen ></iframe>
-            <iframe tabIndex={60} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FW0sPKJyZ5NkTzDm6Bxr470%2FDesigned-Pathway-Page-web-content%3Fnode-id%3D0%253A1" allowFullScreen ></iframe>           
-            <iframe tabIndex={61} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FIRwYu4FleYiFCVJYa5l3Xz%2FFinal-layout%3Fnode-id%3D0%253A1" allowFullScreen ></iframe> 
-            <iframe tabIndex={62} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FKxOCQL5nOLkfeoo9Ohwtok%2FFINAL-COPY-2ND-VARIATION" allowFullScreen ></iframe>
-            <iframe tabIndex={63} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FnOLP6YFpCb2B59PlC5ELbB%2F2-Variations-web-content%3Fnode-id%3D0%253A1" allowFullScreen ></iframe>
+        <div className="container">
+            <div className="SimplySweet">
+                <div className="content">
+                    <h2>Simply Sweet Stems Project</h2>
+                    <h3>Objective & final deliverables (solo project)</h3>
+                    <ul>
+                        <li>Written content for 11 webpages (4 pages was the requirement)</li>
+                        <li>1 high fidelity prototype (not required)</li>
+                        <li>2 completed alternate homepages (not required)</li>
+                        <li>1 persona and supporting scenario</li>
+                        <li>1 pathway page</li>
+                    </ul>
+                    <h3>Personal Goal</h3>
+                    <p>As an aspiring UX designer, I wanted to pick a theme for this new website that challenges me in terms of aesthetics.</p>
+                    <p>I strived to step outside of my comfort zone that is bold and modern with something that is romantic and elegant.</p>
+                    <h3>Timeline</h3>
+                    <ul>
+                        <li>One month</li>
+                    </ul>
+                    <h3>Tools</h3>
+                    <ul>
+                        <li>Figma</li>
+                        <li>Photoshop</li>
+                        <li>Pen</li>
+                        <li>Paper</li>
+                        <li>Sticky notes</li>
+                    </ul>
+                    <h3>Problem statement</h3>
+                    <p>How do I create a wedding floral website for brides and grooms that meets and exceeds their floral expectations while simultaneously addressing COVID-19 policies and procedures?</p>
+                    <h3>Audience</h3>
+                    <ul>
+                        <li>Brides</li>
+                        <li>Grooms</li>
+                        <li>Mother/father of the bride/groom</li>
+                        <li>Maid of honor</li>
+                        <li>Best man</li>
+                    </ul>
+                    <h3>Research</h3>
+                    <ul>
+                        <li>Bride and groom wants, needs, & expectations</li>
+                        <li>Floral expectations</li>
+                        <li>Multiple wedding florist sites fro content</li>
+                        <li>Studied the day and the life of a wedding florist</li>
+                    </ul>
+                    <h3>Concerns</h3>
+                    <ul>
+                        <li>Different types of florals</li>
+                        <li>Floral arrangements</li>
+                        <li>Floral freshness</li>
+                        <li>If florals are local or not</li>
+                        <li>Allergies & pesticides</li>
+                        <li>Environmental impact: silk flowers vs fresh, floral foam, etc</li>
+                        <li>Covid-19 protocols (in-store pickup, curbside, or delivered)</li>
+                    </ul>
+                    <h3>Pain points</h3>
+                    <ul>
+                        <li>Brides</li>
+                        <li>Grooms</li>
+                        <li>Mother/father of the bride/groom</li>
+                        <li>Maid of honor</li>
+                        <li>Best man</li>
+                    </ul>
+                    <h3>Process</h3>
+                    <p>I start with a very preliminary brainstorming technique called mind-mapping. I empty all of my initial ideas, thoughts, and objectives by linking key concepts using images and lines. It helps me flush out my ideas and personal biases.</p>
+                    <img src={Brainstorm} />
+                    <p>Next, I spent the majority of my time researching my audience as it is the foundation for design strategy. A quote that sticks with me states, "Want your users to fall in love with your designs? Fall in love with your users." written by Dana Chisnell. This quote taught me that my sole goal isn't to make websites look "pretty", rather, effectively tailoring users' needs.</p>
+                    <img src={Research} />
+                    <p>In short, I want to provide users' a solution that balances their needs that is functional yet aesthetically pleasing. I research their emotional state, location, needs, wants, etc. After that, I heavily studied competitor websites. I took the time to write down terms that would interest my audience; terms that they are seeking.</p>
+                    <p>In addition, I broke down the hierarchy of the written content page by page, word by word, down to the primary, horizontal, sticky navigation bar. These details matter to me because Jakob's Law states that people spend most of their time using digital products other than yours.</p>
+                    <p>Additionally, I studied various types of wedding floral arrangements, different types of wedding florals, studied the branding approach for this new website, and hypothesized business needs.</p>
+                    <p>With the time limit I was given and the additional pages I wanted to implement, I still added on new research. I omitted original ideas and continuously referenced the research as a whole, keeping in mind user's needs was at the center of this new website.</p>
+                    <p>Lastly, I unfortunately didn’t have the time to create any paper prototypes or low-fidelity prototypes as this was primarily a web writing class.</p>
+                    <h3>Next steps</h3>
+                    <p>If I were to move forward with this project I would meet with the design team and listen to their recommendations. I would point out that my buttons currently on my proposed designs need tailoring. I would like to ensure the shape, placement, and font size are suited for the audience.</p>
+                    <h3>What I learned</h3>
+                    <p>I used to lack confidence in my writing and graphic design skills, but not anymore. Pushing myself beyond the required work in all of my classes has made me a solid aspiring UX designer.</p>
+                    <p>I learned that my projects are so important to me. While I have made much progress in my studies, I still have so much to learn and that excites me.</p>
+                    <h3>Final product</h3>
+                    <iframe tabIndex={59} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Flmzx6Z3zw03JWrQqna5fe9%2FPersona-and-Scenario-rachel-Sadatian" allowFullScreen ></iframe>
+                    <iframe tabIndex={60} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FW0sPKJyZ5NkTzDm6Bxr470%2FDesigned-Pathway-Page-web-content%3Fnode-id%3D0%253A1" allowFullScreen ></iframe>
+                    <iframe tabIndex={61} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FIRwYu4FleYiFCVJYa5l3Xz%2FFinal-layout%3Fnode-id%3D0%253A1" allowFullScreen ></iframe>
+                    <iframe tabIndex={62} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FKxOCQL5nOLkfeoo9Ohwtok%2FFINAL-COPY-2ND-VARIATION" allowFullScreen ></iframe>
+                    <iframe tabIndex={63} style={iframeStyle} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FnOLP6YFpCb2B59PlC5ELbB%2F2-Variations-web-content%3Fnode-id%3D0%253A1" allowFullScreen ></iframe>
+                </div>
+            </div>
         </div>
     );
 }
