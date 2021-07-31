bde06e96b849ba888288a948c4f6210b17184335
Keon <ksadatian@indeed.com>
2021-07-31 16:55:31 -0700

    trying to adjust desktop navbar alignment.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,34 @@ function Header() {
   return (
     <div className="Header">
       <img src={brandLogo} className="brandLogo" />
-      <Navbar bg="#4A3C2F" expand="md">
+      <Navbar expand="md">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+            <Nav>
+              <Nav.Item>
+                <Nav.Link href="/">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/about">About</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <NavDropdown title="Projects" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/portfolio/simply-sweet-stems">Simply Sweet Stems</NavDropdown.Item>
+                  <NavDropdown.Item href="/portfolio/pellegrino">Pellegrino</NavDropdown.Item>
+                  <NavDropdown.Item href="/portfolio/doordash">Doordash</NavDropdown.Item>
+                  <NavDropdown.Item href="/portfolio/designbasics">Design Basics</NavDropdown.Item>
+                </NavDropdown>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/gallery">Gallery</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+              </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Container>
